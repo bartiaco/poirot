cb2236cebc9863c37f03ae108dd653a03e614cea
Brian Artiaco <brian@kingofweb.com>
2011-06-08 16:36:16 -0700

    updating Poirot asset tag helper to better support Icanhas javascript mustache library.

--- lib/poirot/asset_helper.rb
+++ lib/poirot/asset_helper.rb
@@ -1,10 +1,15 @@
 module Poirot
   module AssetHelper
     def template_include_tag(*sources)
+      options = sources.extract_options!.stringify_keys
+      options[:partial] ||= false
+
       sources.collect do |source|
         template_path = Rails.root.join('app/views', controller_name, "_#{source}.html.mustache")
         template = File.open(template_path, "rb")
-        content_tag :script, template.read.html_safe, :type => "text/mustache", :id => "#{source.dasherize}-template"
+        content_options_hash = {:type => "text/html", :id => "#{source.dasherize}"}
+        content_options_hash[:class] = "partial" if options[:partial]
+        content_tag :script, template.read.html_safe, content_options_hash
       end.join("\n").html_safe
     end
   end
